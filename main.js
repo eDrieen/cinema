a1d669fda34f618fe5d04f063273a2c5e63ab964
Adrian Kijewski <kij33q@gmail.com>
2018-09-04 15:57:53 +0200

    Message

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ $('#main_slider').owlCarousel({
         },
         1281: {
             items: 5
+
         }
     }
 });
@@ -33,41 +34,4 @@ $('#timetable_slider').owlCarousel({
             items: 1,
         }
     }
-});
-
-let icon = null;
-let secondmenu = null;
-let box_btn = null;
-let box = null;
-let cont = null;
-
-document.addEventListener("DOMContentLoaded", function () {
-    icon = document.querySelector(".icon");
-    secondmenu = document.querySelectorAll(".secondmenu");
-    box_btn = document.querySelectorAll(".box_icon");
-
-    let plus_element = document.createElement("i");
-    plus_element.classList.add("fas", "fa-plus");
-
-    let minus_element = document.createElement("i");
-    minus_element.classList.add("fas", "fa-minus");
-
-    secondmenu = [...secondmenu];
-
-    let menuStatus = false;
-
-    icon.addEventListener("click", function () {
-        if (!menuStatus) {
-            for (let i = 0; i < secondmenu.length; i++) {
-                secondmenu[i].style.display = "block";
-            }
-            menuStatus = true;
-        } else {
-            for (let i = 0; i < secondmenu.length; i++) {
-                secondmenu[i].style.display = "none";
-            }
-            menuStatus = false;
-        }
-    });
-
 });
